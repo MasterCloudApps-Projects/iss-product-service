691ff548dd6a665a19de2e79ba0a0dbf7e8d26d5
François <fpoirier2002@gmail.com>
2020-12-06 16:03:24 +0100

    @EnableDiscoveryClient declaration

--- src/main/java/es/urjc/code/products/Application.java
+++ src/main/java/es/urjc/code/products/Application.java
@@ -2,8 +2,12 @@ package es.urjc.code.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableMongoRepositories
 public class Application {
 
 	public static void main(String[] args) {
